c8b7254dbe74ce267eb251126363bfae8b6678d8
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-02-06 14:48:21 -0300

    🎉  first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a9f2977eed96145f84eeb69361624bc3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '70015b0a-55a3-4b01-963d-903204b32895'
+};
