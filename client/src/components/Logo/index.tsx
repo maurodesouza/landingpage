360ef1f8e601d7eba08313ba21c0af9a8060f29c
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-02-09 18:54:09 -0300

    📕  (web) class 6 - using environment variables

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { getImageUrl } from 'utils/getImageUrl'
+
 import { LogoProps } from 'types/api'
 import * as S from './styles'
 
 const Logo = ({ alternativeText, url }: LogoProps) => (
-  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+  <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
 export default Logo
