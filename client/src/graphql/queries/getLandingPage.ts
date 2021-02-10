b134e4ddf5600329e3077a123295f8370e06eabc
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-02-09 23:05:04 -0300

    📔  (web) class 7 - add data to section hero

--- client/src/graphql/queries/getLandingPage.ts
+++ client/src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,30 @@
 const query = `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
         alternativeText
         url
       }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
     }
   }
 `
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -19,9 +19,9 @@ import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 
 import { LandingPageProps } from 'types/api'
 
-const Index = ({ logo }: LandingPageProps) => (
+const Index = ({ logo, header }: LandingPageProps) => (
   <>
-    <SectionHero logo={logo} />
+    <SectionHero logo={logo} header={header} />
     <SectionAboutProject />
     <SectionTech />
     <SectionConcepts />
